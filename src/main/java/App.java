904163f5265607dc530364827f13a493673c1ceb
Jeffrey Ruder <ruderjt@gmail.com>
2016-01-14 19:24:51 -0800

    Add code for Heroku deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,8 +8,18 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
 
-      staticFileLocation("/public");
-      String layout = "templates/layout.vtl";
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        setPort(port);
+
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
 
         //RESTful ARCHITECTURE
         //Use POST to create something on the server
